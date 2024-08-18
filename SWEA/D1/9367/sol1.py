1a87fcf076f98c4abf0028e831c2348875a928e9
nahyun <irene.k2547@gmail.com>
2024-08-18 14:08:49 +0900

    9367 / 점점 커지는 당근의 개수 / D1 / 15M / sol1

--- SWEA/D1/9367/sol1.py
+++ SWEA/D1/9367/sol1.py
@@ -12,7 +12,7 @@ for t in range(1, T+1):
     answer = 1  # 최대 연속 증가 당근 수 기록 변수 (출력할 변수)
     cnt = 1     # 연속으로 커지는 당근 수 기록 변수
 
-    for i in range(1, N):  # carrots 순회
+    for i in range(1, N):               # carrots 순회
         if carrots[i-1] < carrots[i]:   # 직전 당근보다 크다면,
             cnt += 1                    # 기록 증가
         else:
