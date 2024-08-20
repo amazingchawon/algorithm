7e760228032ea7abd38e3d79ca4af6f7b878ae0d
nahyun <irene.k2547@gmail.com>
2024-08-20 23:25:20 +0900

    1220 / Magnetic / D3 / 10M / sol2

--- SWEA/D3/1220/sol2.py
+++ SWEA/D3/1220/sol2.py
@@ -14,11 +14,12 @@ for t in range(1, T+1):
     answer = 0
 
     # 교착 상태 수 세기
+    # 0을 뺴고 12 개수 == 교착 상태에 빠진 자석의 수
     for y in range(SIZE):
         col = ''
         for x in range(SIZE):
-            if arr[x][y] != 0:
-                col += str(arr[x][y])
+            if arr[x][y] != 0:          # 0이 아니면
+                col += str(arr[x][y])   # 더해주기
         answer += col.count('12')
 
     print(f'#{t} {answer}')
