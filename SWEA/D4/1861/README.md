1ba1e2120ae9f8f4a104888be9831bc3c811f5f7
nahyun <irene.k2547@gmail.com>
2024-09-12 15:53:14 +0900

    1861 / 정사각형 방 / D4 / 40M / sol1, README

---
+++ SWEA/D4/1861/README.md
@@ -0,0 +1,11 @@
+# [[D4] 1861 정사각형 방](https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZCW2rDqGb4DFAUC&contestProbId=AV5LtJYKDzsDFAXc&probBoxId=AZG6SCa6jS8DFAXd&type=PROBLEM&problemBoxTitle=%EA%B7%B8%EB%9E%98%ED%94%84+III&problemBoxCnt=8)
+
+## 연관 개념
+[DFS](https://github.com/amazingchawon/TIL/blob/master/Algorithm/DFS.md)
+
+## 새롭게 배운 것
+1. 2차원 배열에서 max 값 찾기
+   ```python
+   arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+   max_num = max(map(max, arr))
+   ```
---
+++ SWEA/D4/1861/sol1.py
@@ -0,0 +1,41 @@
+# [D4] 1861 정사각형 방
+
+import sys
+sys.stdin = open('input.txt', 'r')
+
+def DFS(V):
+    stack = [V]
+
+    while stack:
+        x, y = stack.pop()
+        for dx, dy in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < N and arr[x][y] + 1 == arr[nx][ny]:
+                stack.append((nx, ny))
+                level[nx][ny] = level[x][y] + 1
+                break                   # 숫자가 모두 다르므로, 한 방향으로 밖에 탐색 불가
+
+    return level[x][y]
+
+
+T = int(input())
+
+for t in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    room_num = 0
+    max_move = 0
+    level = [[0] * N for _ in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            level[i][j] = 1
+            tmp_room, tmp_move = arr[i][j], DFS((i, j))
+            if max_move < tmp_move:                 # 최대 움직인 방 수 보다 현재 위치에서 움직인 방 수가 더 많으면,
+                room_num = tmp_room                 # 움직인 방 번호 갱신
+                max_move = tmp_move                 # 최대 움직인 방 수 갱신
+            elif max_move == tmp_move:              # 동일하다면,
+                room_num = min(room_num, tmp_room)  # 움직인 방 번호 중 작은 걸로 갱신 (이거 안해줘도 되긴함)
+
+    print(f'#{t} {room_num} {max_move}')
